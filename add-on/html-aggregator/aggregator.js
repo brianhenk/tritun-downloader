19462d3d4f969010a4f08c4131316153a630b9ed
Brian Henk <code@brianhenk.com>
2018-03-24 12:51:11 -0400

    Factor out some methods

--- add-on/html-aggregator/aggregator.js
+++ add-on/html-aggregator/aggregator.js
@@ -3,23 +3,48 @@ browser.tabs.getCurrent().then((currentTab) => {
         subject: "get-document",
         tabId: currentTab.openerTabId
     }).then((doc) => {
-        let titleElement = document.getElementById("doc-title");
-        titleElement.textContent = doc.title;
-        document.title = doc.title;
-
-        let countFoundSpan = document.getElementById("count-found");
-        countFoundSpan.textContent = doc.htmlFiles.length;
-
-        let progressDiv = document.getElementById("progress");
-        let countDoneSpan = document.getElementById("count-done");
-        let percentDoneSpan = document.getElementById("percent-done");
-        let minsRemainingSpan = document.getElementById("mins-remaining");
-
-        let loadingDiv = document.getElementById("doc-loading");
         let contentDiv = document.getElementById("doc-content");
 
+        function setTitle(title) {
+            let titleElement = document.getElementById("doc-title");
+            titleElement.textContent = title;
+            document.title = title;
+        }
+
+        function incrementProgress(found, inc) {
+            document.getElementById("count-found").textContent = found;
+
+            let countDoneSpan = document.getElementById("count-done");
+            let done = parseInt(countDoneSpan.textContent) + inc;
+            countDoneSpan.textContent = done.toFixed(0);
+
+            let percentDone = (done / found) * 100;
+            document.getElementById("percent-done").textContent = percentDone.toFixed(1);
+            document.getElementById("progress").style.width = percentDone.toFixed(0) + "%";
+
+            let minutesRemaining = (found - done) * 1800 / 1000 / 60;
+            document.getElementById("mins-remaining").textContent = minutesRemaining.toFixed(1);
+        }
+
+        function revealAndDownload() {
+            document.getElementById("doc-loading").setAttribute("style", "display:none;");
+            document.getElementById("doc-content").setAttribute("style", "");
+            browser.tabs.saveAsPDF({
+                showBackgroundColors: true,
+                headerLeft: "",
+                headerRight: "",
+                footerLeft: "&T",
+                footerRight: "&PT"
+            });
+        }
+
+        setTitle(doc.title);
+
+        let filesCount = doc.htmlFiles.length;
+        incrementProgress(filesCount, 0);
+
         let previousProcedures = [];
-        for(let i = 0; i < doc.htmlFiles.length; i++) {
+        for(let i = 0; i < filesCount; i++) {
             let file = doc.htmlFiles[i];
 
             setTimeout(() => {
@@ -39,22 +64,22 @@ browser.tabs.getCurrent().then((currentTab) => {
                 previousProcedures = file.procedures;
 
                 // Fetch content
-                fetch(file.url, {credentials: "include"}).then((response) => {
-                    return response.text();
-                }).then((text) => {
-                    let tempDoc = new DOMParser().parseFromString(text, "text/html");
+                fetch(file.url, { credentials: "include" }).then((fileResponse) => {
+                    return fileResponse.text();
+                }).then((fileText) => {
+                    let fileDoc = new DOMParser().parseFromString(fileText, "text/html");
 
                     // Remove h1
-                    let primaryHeader = tempDoc.querySelector("h1");
+                    let primaryHeader = fileDoc.querySelector("h1");
                     if (primaryHeader != null) {
                         primaryHeader.remove();
                     }
 
                     // Lower heading level for other headers
                     for (let headerLevel = 6; headerLevel > 1; headerLevel--) {
-                        let headers = tempDoc.querySelectorAll("h" + headerLevel);
+                        let headers = fileDoc.querySelectorAll("h" + headerLevel);
                         for (let header of headers) {
-                            let newHeader = tempDoc.createElement("h" + (lowestHeaderLevel + headerLevel - 1));
+                            let newHeader = fileDoc.createElement("h" + (lowestHeaderLevel + headerLevel - 1));
                             newHeader.innerHTML = header.innerHTML;
                             header.insertAdjacentElement('afterend', newHeader);
                             header.remove();
@@ -62,24 +87,24 @@ browser.tabs.getCurrent().then((currentTab) => {
                     }
 
                     // Remove internal links
-                    let links = tempDoc.querySelectorAll("a");
+                    let links = fileDoc.querySelectorAll("a");
                     for (let link of links) {
                         link.insertAdjacentHTML('afterend', link.innerHTML);
                         link.remove();
                     }
 
                     // Replace SVG objects with images
-                    let svgDivs = tempDoc.querySelectorAll("div.svg-pan-zoom");
+                    let svgDivs = fileDoc.querySelectorAll("div.svg-pan-zoom");
                     for (let svgDiv of svgDivs) {
                         let object = svgDiv.querySelector("object");
-                        let image = tempDoc.createElement("img");
+                        let image = fileDoc.createElement("img");
                         image.setAttribute("src", new URL(object.getAttribute("data"), "https://www.tritun.net").toString());
                         svgDiv.insertAdjacentElement("afterend", image);
                         svgDiv.remove();
                     }
 
                     // Replace IMG objects with images
-                    let imgSections = tempDoc.querySelectorAll("section.img-pan-zoom");
+                    let imgSections = fileDoc.querySelectorAll("section.img-pan-zoom");
                     for (let imgSection of imgSections) {
                         let img = imgSection.querySelector("img");
                         img.setAttribute("src", new URL(img.getAttribute("src"), "https://www.tritun.net").toString());
@@ -88,29 +113,18 @@ browser.tabs.getCurrent().then((currentTab) => {
                     }
 
                     // Change images to absolute urls
-                    let images = tempDoc.querySelectorAll("img");
+                    let images = fileDoc.querySelectorAll("img");
                     for (let image of images) {
                         let src = image.getAttribute("src");
                         image.setAttribute("src", new URL(src, "https://www.tritun.net").toString());
                     }
 
-                    lowestHeader.insertAdjacentHTML('afterend', tempDoc.body.innerHTML);
-                    countDoneSpan.textContent = parseInt(countDoneSpan.textContent) + 1;
-                    percentDoneSpan.textContent = ((parseFloat(countDoneSpan.textContent) / parseFloat(countFoundSpan.textContent)) * 100).toFixed(0);
-                    progressDiv.style.width = percentDoneSpan.textContent + "%";
-                    minsRemainingSpan.textContent = ((parseInt(countFoundSpan.textContent) - parseInt(countDoneSpan.textContent)) * 1800 / 1000 / 60).toFixed(1);
+                    lowestHeader.insertAdjacentHTML('afterend', fileDoc.body.innerHTML);
+                    incrementProgress(filesCount, 1);
                 });
 
-                if(i >= doc.htmlFiles.length - 1) {
-                    loadingDiv.setAttribute("style", "display:none;");
-                    contentDiv.setAttribute("style", "");
-                    browser.tabs.saveAsPDF({
-                        showBackgroundColors: true,
-                        headerLeft: "",
-                        headerRight: "",
-                        footerLeft: "&T",
-                        footerRight: "&PT"
-                    });
+                if(i >= filesCount - 1) {
+                    revealAndDownload();
                 }
             }, 1500 * i);
         }
